daf898aae6ef5dbf7f7c90d1259296aee2f166ff
Mouaad Elkhadari <mouaad.elkhadari@gmail.com>
2024-07-14 15:34:10 +0100

    spring boot second commit

---
+++ src/main/java/com/spproject/spproject/Coach.java
@@ -0,0 +1,6 @@
+package com.spproject.spproject;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ src/main/java/com/spproject/spproject/CricketCoach.java
@@ -0,0 +1,11 @@
+package com.spproject.spproject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+
+    public String getDailyWorkout(){
+        return "Practice fast bowling for 15 minutes";
+    }
+}
