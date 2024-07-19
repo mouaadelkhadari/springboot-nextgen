42dd0dbc3f3bb6e2a66cd9e0695b8a901abebb5b
Mouaad Elkhadari <mouaad.elkhadari@gmail.com>
2024-07-19 10:24:08 +0100

    commit number 12

--- src/main/java/com/spproject/spproject/SpprojectApplication.java
+++ src/main/java/com/spproject/spproject/SpprojectApplication.java
@@ -1,8 +1,9 @@
 package com.spproject.spproject;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
 
 
 /* @SpringBootApplication(
@@ -18,5 +19,13 @@ public class SpprojectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpprojectApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("Hello World");
+		};
+	}
+
 
 }
